26e887109280e892d8667fa77cdd2bd05da7f428
suryaprasanthkrish <60041174+suryaprasanthkrish@users.noreply.github.com>
2022-07-17 15:42:41 +0530

    Update confirmed_dash.py

--- confirmed_dash.py
+++ confirmed_dash.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import plotly.offline as pyo
 import plotly.graph_objects as go #geographical heat maps
 import dash
-import dash_html_components as html 
-import dash_core_components as dcc 
+from dash import html 
+from dash import dcc 
 from dash.dependencies import Input, Output
 from datetime import datetime
 
